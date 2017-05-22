680ee274e0eb0376e93abff035b4410130c3deb6
Clint <clintp1@gmail.com>
2017-05-22 20:03:42 +1000

    added test for validations of post that will include a title and body

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-describe Post do
-  it 'should validate presence of title' do
-    post = Post.new
-    post.valid?
-    expect(post.errors.messages[:title]).to include "can't be blank"
+describe 'Validations' do
+  subject(:post) { Post.new } 
+  before { post.valid? }
+
+  [:title, :body].each do |attribute|
+    it "should validate presence of #{attribute}" do
+      expect( post.errors[attribute].size).to be >= 1
+      expect( post.errors.messages[attribute] ).to include "can't be blank"
+    end
   end
 end
